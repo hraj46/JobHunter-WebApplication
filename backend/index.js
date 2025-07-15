fd32a779d0fc95b3daaf7c515ca8f8c650c7cfc2
Himanshu Raj <rajhimanshu7452@gmail.com>
2025-07-15 13:28:46 +0530

    all bug fixes

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,41 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config({});
+
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+const whitelist = [process.env.FRONTEND_URL];
+console.log("Whitelist:", whitelist);
+
 const corsOptions = {
-    origin: process.env.frontend_URL,
-    credentials:true
-}
+  origin: function (origin, callback) {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+};
+
 
 app.use(cors(corsOptions));
+app.use(cookieParser());
+
 
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -32,9 +44,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
